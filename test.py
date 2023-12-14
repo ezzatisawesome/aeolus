384e5447e6dd3c93b5fe393cffe740129a7f3d7e
ezzatisawesome <ezzatsuhaime@gmail.com>
2023-12-13 22:56:12 -0800

    Spacecraft weight

--- test.py
+++ test.py
@@ -5,22 +5,40 @@ import matplotlib.pyplot as plt
 
 m = GEKKO()
 
-# Model parameters
+### Model parameters
+# Earth
 R_reenty = m.Param(value = 6455) # km
-earth_mu = m.Param(value=3.9860043543609598e5) # km^3/s^2
-isp = m.Param(value=200) # s
 g0 = m.Param(value=9.81) # m/s^2
-sc_dry_mass = m.Param(value=1000) # kg
+earth_mu = m.Param(value=3.9860043543609598e5) # km^3/s^2
+atmospheric_density = m.Param(value=1e-12) # kg/m^3
 
-# Model variables
+# Spacecraft
+isp = m.Param(value=200) # s
+sc_dry_mass = m.Param(value=1000) # kg
+coefficient_drag = m.Param(value=2.2) # -
+sc_cross_sectional_area = m.Param(value=3.8) # m^2
+
+
+### Model variables
+# Earth
 sma = m.Var(value=7000, lb=6000, ub=8000) # km
+
+# Spacecraft
 sc_wet_mass = m.Var() # kg
 
-# Intermediate variables
-orbital_velocity = m.Intermediate(m.sqrt(earth_mu / sma)) # km/s
+### Intermediate variables
+# Earth
 V_reentry = m.Intermediate(m.sqrt(earth_mu / R_reenty)) # km/s
+
+# Spacecraft
+orbital_velocity = m.Intermediate(m.sqrt(earth_mu / sma)) # km/s
+orbit_period = m.Intermediate(2 * math.pi * sma / orbital_velocity) # s
 delta_v_deorbit = m.Intermediate(orbital_velocity - V_reentry) # km/s
 propellant_mass = m.Intermediate(m.exp(delta_v_deorbit / (isp * g0)) * sc_dry_mass - sc_dry_mass) # kg
+
+pert_drag_sma = m.Intermediate(-1 * sma**2 * atmospheric_density * coefficient_drag * sc_cross_sectional_area / (2 * math.pi * sc_wet_mass)) # km/s
+number_of_burns = m.Intermediate(pert_drag_sma)
+
 
 # Implicit equations
 m.Equation(sc_wet_mass == sc_dry_mass + propellant_mass)
