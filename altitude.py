cfa8085dda9ace9df0016a0398d377e01c1c92a6
ezzatisawesome <ezzatsuhaime@gmail.com>
2023-12-14 16:12:26 -0800

    Push

--- altitude.py
+++ altitude.py
@@ -1,82 +1,67 @@
-import numpy as np
-from gekko import GEKKO
-import math
-import matplotlib.pyplot as plt
 from atmos1976 import density
-
-m = GEKKO()
-
-### Model parameters
-# Earth
-R_reenty = m.Param(value = 6455) # km
-g0 = m.Param(value=9.80665) # m/s^2
-mu_earth = m.Param(value=3.9860043543609598e5) # km^3/s^2
-R_earth = m.Param(value=6371) # km
-
-# Spacecraft
-isp = m.Param(value=200) # s
-sc_dry_mass = m.Param(value=1300) # kg
-coefficient_drag = m.Param(value=2.2) # -
-sc_cross_sectional_area = m.Param(value=3.8) # m^2
-mission_lifepsan = m.Param(value=3.45) # years
+import math
+import sys
 
 
-### Model variables
-# Earth
-sma = m.Var(value=6750, lb=6378, ub=7200) # km
-
-# Spacecraft
-# sc_wet_mass = m.Var() # kg
-propellant_mass = m.Var() # kg
+# CONSTANTS
+R_reenty = 6455 # km
+R_earth = 6371 # km
+earth_mu = 3.9860043543609598e5 # km^3/s^2
+g0 = 9.80665 # m/s^2
 
 
-### Intermediate variables
-# Earth
-V_reentry = m.Intermediate(m.sqrt(mu_earth / R_reenty)) # km/s
-atmospheric_density = m.Intermediate(density(sma.value - R_earth.value)) # kg/km^3
+def calculate_mass_prop(delta_v, isp, sc_mass):
+    return math.exp(delta_v / (isp * g0 / 1000)) * sc_mass - sc_mass
 
-# Spacecraft
-orbital_velocity = m.Intermediate(m.sqrt(mu_earth / sma)) # km/s
-#- Deorbit
-delta_v_deorbit = m.Intermediate(V_reentry - orbital_velocity) # km/s
-#- Maintenance
-orbit_period = m.Intermediate(2 * math.pi * m.sqrt(sma ** 3 / mu_earth)) # s
-orbits_per_year = m.Intermediate(365 * 24 * 60 * 60 / orbit_period) # orbits / year
-delta_v_lost_drag = m.Intermediate((math.pi * (coefficient_drag * sc_cross_sectional_area / sc_dry_mass) * atmospheric_density * (sma*1000) * (orbital_velocity*1000)) / 1000) # km/rev
-delta_v_maintenance = m.Intermediate(delta_v_lost_drag * orbits_per_year * mission_lifepsan) # km
-#- Propellant
-delta_v_total = m.Intermediate(delta_v_deorbit + delta_v_maintenance) # km
-# propellant_mass = m.Intermediate(m.exp(delta_v_total / (isp * g0 / 1000)) * sc_dry_mass - sc_dry_mass) # kg
+def simulate(altitude):
+    coefficient_drag = 2.2 # -
+    sma = altitude + 6371 # km
+    sc_cross_sectional_area = 28 # m^2
+    sc_dry_mass = 1300 # kg
+    mission_lifepsan = 3.45 # years
+    isp = 200 # s
 
-### Implicit equations
-# m.Equation(sc_wet_mass == sc_dry_mass + propellant_mass)
-m.Equation(propellant_mass == m.exp(delta_v_total / (isp * g0 / 1000)) * sc_dry_mass - sc_dry_mass)
-m.Equation(propellant_mass > 0)
-m.Equation(sma > R_reenty)
+    # Intermediate variables
+    orbital_velocity = math.sqrt(earth_mu / sma) # km/s
+    atmospheric_density = density(sma - R_earth) # kg/km^3
+
+    # Deorbit delta-v
+    V_reentry = math.sqrt(earth_mu / R_reenty) # km/s
+    delta_v_deorbit = V_reentry - orbital_velocity # km/s
+
+    # Maintenance delta-v
+    orbit_period = 2 * math.pi * math.sqrt(sma ** 3 / earth_mu)
+    orbits_per_year = 365 * 24 * 60 * 60 / orbit_period # orbits / year
+    delta_v_lost_drag = (math.pi * (coefficient_drag * sc_cross_sectional_area / sc_dry_mass) * atmospheric_density * (sma*1000) * (orbital_velocity*1000)) / 1000 # km/rev
+    delta_v_maintenance = delta_v_lost_drag * orbits_per_year * mission_lifepsan # km
+
+    # Propellant mass
+    delta_v_total = delta_v_deorbit + delta_v_maintenance # km
+    mass_prop_total = math.exp(delta_v_total / (isp * g0 / 1000)) * sc_dry_mass - sc_dry_mass
+    mass_prop_deorbit = math.exp(delta_v_deorbit / (isp * g0 / 1000)) * sc_dry_mass - sc_dry_mass
+    mass_prop_maintenance = math.exp(delta_v_maintenance / (isp * g0 / 1000)) * sc_dry_mass - sc_dry_mass
+
+    return mass_prop_total, mass_prop_deorbit, mass_prop_maintenance, atmospheric_density
 
 
-### Minimize
-m.Minimize(propellant_mass)
+if __name__ == "__main__":
+    best_altitude = 0
+    best_mass_prop = sys.maxsize
 
+    for i in range(200, 600, 1):
+        mass_prop_total, mass_prop_deorbit, mass_prop_maintenance, atmospheric_density = simulate(i)
 
-### Solve
-m.solve()
+        if mass_prop_total < best_mass_prop:
+            best_mass_prop = mass_prop_total
+            best_altitude = i
 
+        print("Altitude: ", i)
+        print("Total Prop: ", mass_prop_total)
+        print("Mass prop Deorbit: ", mass_prop_deorbit)
+        print("Mass prop Maintenance: ", mass_prop_maintenance)
+        print("Atmospheric Density: ", atmospheric_density)
+        print("\n")
 
-### Print solution
-print('Optimal Solution (m)')
-print('sma: ' + str(sma.value))
-print('Altitude: ' + str(sma.value[0] - R_earth.value[0]))
-# print('sc_wet_mass: ' + str(sc_wet_mass.value))
-print('sc_dry_mass: ' + str(sc_dry_mass.value))
-print('propellant_mass: ' + str(propellant_mass.value))
-print('orbital_velocity: ' + str(orbital_velocity.value))
-print('reentry_velocity: ' + str(V_reentry.value))
-print('delta_v_deorbit: ' + str(delta_v_deorbit.value))
-# - Maintenance
-print('orbit_period: ' + str(orbit_period.value))
-print('orbits_per_year: ' + str(orbits_per_year.value))
-print('Delta V lost per orbit: ' + str(delta_v_lost_drag.value))
-print('delta_v_maintenance: ' + str(delta_v_maintenance.value))
-print('atmospheric_density: ' + str(atmospheric_density.value))
-print('delta_v_total: ' + str(delta_v_total.value))
+    print("\n\n")
+    print("Best Altitude: ", best_altitude)
+    print("Best Prop: ", best_mass_prop)
